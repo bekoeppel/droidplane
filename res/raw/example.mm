c0674cad426f8fe005865f01b9387b6b2f93cf19
Benedikt Koeppel <be.public@gmail.com>
2013-03-14 07:56:21 +0100

    Code clean up
    - comments
    - better help

--- res/raw/example.mm
+++ res/raw/example.mm
@@ -1,14 +1,38 @@
 <map version="0.9.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Help" ID="ID_1385587321" CREATED="1361978276662" MODIFIED="1363198391078">
+<node TEXT="DroidPlane Help" ID="ID_1385587321" CREATED="1361978276662" MODIFIED="1363243426259">
 <hook NAME="MapStyle" max_node_width="600"/>
 <node TEXT="Opening Files" POSITION="right" ID="ID_564005984" CREATED="1363198401582" MODIFIED="1363198403584">
-<node TEXT="In Dropbox, open the &quot;.mm&quot; document" ID="ID_1466149708" CREATED="1363198405484" MODIFIED="1363198418681"/>
-<node TEXT="You might be asked with which application you want to open your MindMap" ID="ID_691670280" CREATED="1363198418886" MODIFIED="1363198429079"/>
-<node TEXT="Choose DroidPlane" ID="ID_1112539367" CREATED="1363198429265" MODIFIED="1363198432369"/>
+<node TEXT="DroidPlane can be launched from any file manager application. For example, you can open Mindmap files from Dropbox, Root Explorer, ASTRO File Manager and others." ID="ID_1932877744" CREATED="1363243479606" MODIFIED="1363243823601"/>
+<node TEXT="Simply locate your Mindmap document (ending in &quot;.mm&quot;) and tap the document." ID="ID_1348665736" CREATED="1363243532552" MODIFIED="1363243556485"/>
+<node TEXT="You might be asked with which application you want to open your MindMap: simply choose DroidPlane" ID="ID_691670280" CREATED="1363198418886" MODIFIED="1363243569404"/>
+</node>
+<node TEXT="Navigating" POSITION="right" ID="ID_1112944485" CREATED="1363243720200" MODIFIED="1363243721951">
+<node TEXT="Once you have opened a Mindmap, you will see the root node of your Mindmap." ID="ID_815653366" CREATED="1363243722626" MODIFIED="1363243839151"/>
+<node TEXT="Tapping a node will open this node, and display all its child nodes." ID="ID_1446105429" CREATED="1363243839703" MODIFIED="1363243860971"/>
+<node TEXT="To navigate back up in the Mindmap, you can press the back button or the application button in the upper left corner." ID="ID_409514355" CREATED="1363243861212" MODIFIED="1363243884810"/>
+</node>
+<node TEXT="Editing" POSITION="right" ID="ID_1832587474" CREATED="1363243888925" MODIFIED="1363243889965">
+<node TEXT="Editing is currently not possible. But it is on the list of Future Features." ID="ID_400456261" CREATED="1363243890193" MODIFIED="1363243906057">
+<arrowlink DESTINATION="ID_624973113" STARTINCLINATION="338;0;" ENDINCLINATION="338;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
 </node>
 <node TEXT="About" POSITION="right" ID="ID_495169311" CREATED="1363198391806" MODIFIED="1363198394109">
-<node TEXT="Written by Benedikt K&#xf6;ppel" ID="ID_1678888380" CREATED="1363198433472" MODIFIED="1363198439036"/>
+<node TEXT="Author" ID="ID_1678888380" CREATED="1363198433472" MODIFIED="1363243586941">
+<node TEXT="DroidPlane is developed by Benedikt K&#xf6;ppel" ID="ID_825321977" CREATED="1363243594597" MODIFIED="1363243600965"/>
+<node TEXT="Benedikt K&#xf6;ppel&#xa;code@benediktkoeppel.ch&#xa;http://www.benediktkoeppel.ch" ID="ID_721590164" CREATED="1363243605288" MODIFIED="1363243616253"/>
+</node>
+<node TEXT="Future Features" ID="ID_624973113" CREATED="1363243755234" MODIFIED="1363243760352">
+<node TEXT="Display icons, links," ID="ID_1553267857" CREATED="1363243776335" MODIFIED="1363243795960"/>
+<node TEXT="Copy the text of a node" ID="ID_1543180359" CREATED="1363243796156" MODIFIED="1363243804355"/>
+<node TEXT="Support for editing Mindmaps" ID="ID_601546453" CREATED="1363243760701" MODIFIED="1363243776088"/>
+</node>
+<node TEXT="Feedback" ID="ID_1851185539" CREATED="1363243592512" MODIFIED="1363243593836">
+<node TEXT="If you want to report a bug, have any suggestions how to improve DroidPlane, or want to ask for a new feature, please drop me a line to code@benediktkoeppel.ch" ID="ID_1770042450" CREATED="1363243624837" MODIFIED="1363243669762"/>
+</node>
+<node TEXT="Licence" ID="ID_1972871592" CREATED="1363243587117" MODIFIED="1363243588125">
+<node TEXT="Haven&apos;t decided yet." ID="ID_1584402594" CREATED="1363243685004" MODIFIED="1363243716144"/>
+</node>
 </node>
 </node>
 </map>
--- src/ch/benediktkoeppel/code/droidplane/MainActivity.java
+++ src/ch/benediktkoeppel/code/droidplane/MainActivity.java
@@ -35,28 +35,32 @@ public class MainActivity extends Activity implements OnItemClickListener {
 	
 	private static final String TAG = "DroidPlane";
 
+	// GUI stuff
+	// Menu identifiers
 	private static final int MENU_UP = 0;
 	
-	InputStream mm;
+	// Components
 	ListView listView;
+	ArrayAdapter<String> adapter;
+
 
 	
+	// Mindmap stuff
+	InputStream mm;
+	// XML document
 	DocumentBuilderFactory docBuilderFactory;
 	DocumentBuilder docBuilder;
 	Document document;
-	
+	// stack of parent nodes
 	Stack<Node> parents = new Stack<Node>();
+	// the latest parent node (all visible nodes are childs of this currentParent
+	// TODO: shouldn't this be the same as parents.peek()?
 	Node currentParent;
+	// the currently visible nodes
 	ArrayList<Node> currentListedNodes = new ArrayList<Node>();
+	// the text of the currently visible nodes
 	ArrayList<String> str_currentListedNodes = new ArrayList<String>();
 	
-	
-	
-	@Override
-	public boolean onCreateOptionsMenu(android.view.Menu menu) {
-		menu.add(0, MENU_UP, 0, "Up");
-		return true;
-	}
 	
     @SuppressLint("NewApi")
 	@Override
@@ -64,24 +68,30 @@ public class MainActivity extends Activity implements OnItemClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        
+        // initialize android stuff
+        // components
         listView = (ListView)findViewById(R.id.list_view);
-
+    	listView.setOnItemClickListener(this);
+    	
+        // menu bar: if we are at least at API 11, the Home button is kind of a back button in the app
     	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 	    	ActionBar bar = getActionBar();
 	    	bar.setDisplayHomeAsUpEnabled(true);
     	}
     	
-    	
+    	// intents (how we are called)
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
 
-        InputStream mm = null;
         
-        
+        // determine whether we are started from the EDIT or VIEW intent, or whether we are started from the launcher
+        // started from ACTION_EDIT/VIEW intent
         if ((Intent.ACTION_EDIT.equals(action)||Intent.ACTION_VIEW.equals(action)) && type != null) {
         	
+        	Log.d(TAG, "started from ACTION_EDIT/VIEW intent");
+        	
+        	// get the URI to the target document (the Mindmap we are opening) and open the InputStream
         	Uri uri = intent.getData();
         	if ( uri != null ) {
         		ContentResolver cr = getContentResolver();
@@ -92,21 +102,21 @@ public class MainActivity extends Activity implements OnItemClickListener {
 					e.printStackTrace();
 				}
         	}
-  
-      
-        } else {
-	
-	        
-	        
-	        
-	        Log.e(TAG, "Before opening resource");
-	        
+        } 
+        
+        // started from the launcher
+        else {
+        	
+        	Log.d(TAG, "started from app launcher intent");
+        	
+        	// display the default Mindmap "example.mm", from the resources
 	    	mm = this.getResources().openRawResource(R.raw.example);
-	    	
         }
         
-        docBuilderFactory = DocumentBuilderFactory.newInstance();
+        Log.d(TAG, "InputStream fetched, now starting to load document");
         
+        // load the Mindmap from the InputStream
+        docBuilderFactory = DocumentBuilderFactory.newInstance();
 		try {
 			docBuilder = docBuilderFactory.newDocumentBuilder();
 			document = docBuilder.parse(mm);
@@ -121,20 +131,31 @@ public class MainActivity extends Activity implements OnItemClickListener {
 			return;
 		}
 		
+		Log.d(TAG, "Document loaded");
 		
-		
-		Log.e(TAG, "After opening resource");
 
+		// TODO: in the beginning, don't only display the root node. display it as if the root node was already opened
+		// TODO: in the menu bar, the name of the root node should be displayed (truncated to fit the width)
     	currentParent = document.getDocumentElement();
 		listChildren();
         
     }
 
-    
-    public void goUp(View view) {
-    	up();
-    }
-    
+    // sets the menu
+    @Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+
+    	// TODO: shouldn't this be in some kind of menu.xml?
+    	// add one menu item "Up"
+    	// TODO: what does this line actually do? what do the two 0 mean?
+		menu.add(0, MENU_UP, 0, "Up");
+		
+		// TODO: add "Go to Top" button
+		// TODO: add "Find" button and menu -> should search underneath the current node (or with an option, under the root node)
+		return true;
+	}
+
+    // navigates back up one level in the Mindmap
     public void up() {
 		if ( parents.size() > 0 ) {
 			currentParent = parents.pop();
@@ -142,12 +163,63 @@ public class MainActivity extends Activity implements OnItemClickListener {
 		} 
     }
     
+    
+    
+    // TODO: documentation
+    // TODO: cleanup
+    // TODO: has a lot of side effects...! :-) (nothing else than...)
+    private void listChildren() {
+    	
+    	Log.e(TAG, "listChildren called");
+    	
+    	// create new, empty ArrayLists
+    	str_currentListedNodes = new ArrayList<String>();
+    	currentListedNodes = new ArrayList<Node>();
+    	
+    	// fetch the children nodes of the current parent
+    	NodeList tmp_children = currentParent.getChildNodes();
+    	for (int i = 0; i < tmp_children.getLength(); i++) {
+    		Node tmp_n = tmp_children.item(i);
+    		
+    		// only interested in the elements (no comments, etc.)
+    		if ( tmp_n.getNodeType() == Node.ELEMENT_NODE ) {
+    			Element tmp_e = (Element)tmp_n;
+
+    			// TODO: handle other tags, such as icons, links etc.
+    			
+    			// only interested in nodes called "node"
+    			if ( tmp_e.getTagName().equals("node") ) {
+    				
+    				// extract the string (TEXT attribute) of the nodes
+	    			str_currentListedNodes.add(tmp_e.getAttribute("TEXT"));
+	    			currentListedNodes.add(tmp_e);
+    			}
+    		}
+		}
+    	
+    	// create adapter (i.e. data provider) for the listView
+    	// TODO: maybe we need a special list item layout, so that we can show links, icons and text
+    	adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, str_currentListedNodes);
+    	listView.setAdapter(adapter); 
+    	  	
+    }
+
+    
+    
+    // Handler of all menu events
+    // Home button: navigate one level up, and exit the application if the home button is pressed at the root node
+    // Menu Up: navigate one level up, and stay at the root node
     @Override
     public boolean onOptionsItemSelected(android.view.MenuItem item) {
     	
     	switch (item.getItemId()) {
+    	
+    	// "Up" menu action
     	case MENU_UP:
     		up();
+    		break;
+    		
+		// App button (top left corner)
     	case android.R.id.home:
         	if ( parents.size() > 0 ) {
     			currentParent = parents.pop();
@@ -155,58 +227,31 @@ public class MainActivity extends Activity implements OnItemClickListener {
         	} else {
         		finish();
         	}
+        	break;
     	}
+    	
     	return true;
     }
     
+    // Handler for the back button
+    // Navigate one level up, and stay at the root node
     @Override
     public void onBackPressed() {
     	up();
     }
     
-    private void listChildren() {
-    	
-
-    	Log.e(TAG, "listChildren called");
-    	
-    	str_currentListedNodes = new ArrayList<String>();
-    	currentListedNodes = new ArrayList<Node>();
-    	
-    	NodeList tmp_children = currentParent.getChildNodes();
-    	int tmp_children_n = tmp_children.getLength();
-    	
-    	for (int i = 0; i < tmp_children_n; i++) {
-    		Node tmp_n = tmp_children.item(i);
-    		
-    		if ( tmp_n.getNodeType() == Node.ELEMENT_NODE ) {
-    			Element tmp_e = (Element)tmp_n;
-
-    			if ( tmp_e.getTagName().equals("node") ) {
-	    			str_currentListedNodes.add(tmp_e.getAttribute("TEXT"));
-	    			currentListedNodes.add(tmp_e);
-    			}
-    		}
-    					
-		}
-    	
-    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, str_currentListedNodes);
-    	listView.setAdapter(adapter); 
-    	listView.setOnItemClickListener(this);
-
-
-    	  	
-    }
-
+    // Handler when one of the ListItem's item is clicked
+    // Find the node which was clicked, and redraw the screen with this node as new parent
+    // TODO: menu items should be clickable across the full width!
+    // TODO: menu items with no children should not be clickable
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 	
 		parents.push(currentParent);
 		currentParent = currentListedNodes.get(position);
 		
-		
 		listChildren();
-		
-		
 	}
+
 
 }
