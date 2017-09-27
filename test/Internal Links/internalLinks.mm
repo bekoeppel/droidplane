d8c83d61830567a6941580f75ede6d1bc9d4eb6f
Thomas Kluth <t.kluth@posteo.de>
2017-09-27 16:10:30 +0200

    added support for mindmap-internal links

---
+++ test/Internal Links/internalLinks.mm
@@ -0,0 +1,68 @@
+<map version="docear 1.1" dcr_id="1506506317261_cyeqzyfwqaciwon38hv5q6re0" project="14600A490F82GW74HCI0D5F8BHER32FQFK1V" project_last_home="file:/home/thomas/Dokumente/citec/literatur/">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="test internal links" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1506506326440"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="level1" POSITION="right" ID="ID_1333607927" CREATED="1506506327770" MODIFIED="1506506331695">
+<edge COLOR="#ff0000"/>
+<node TEXT="level2a" ID="ID_1015016317" CREATED="1506506332372" MODIFIED="1506521364293">
+<node TEXT="level3a" ID="ID_1236717300" CREATED="1506506344681" MODIFIED="1506521356257">
+<node TEXT="level4a" ID="ID_1928725575" CREATED="1506521380175" MODIFIED="1506521388126"/>
+</node>
+</node>
+<node TEXT="level2b" ID="ID_1347930958" CREATED="1506506335234" MODIFIED="1506521365954">
+<node TEXT="level3b, links to level 3a" ID="ID_1202268203" CREATED="1506506340485" MODIFIED="1506521377916" LINK="internalLinks.mm#ID_1236717300">
+<node TEXT="level4b" ID="ID_1880316730" CREATED="1506521391192" MODIFIED="1506521393653"/>
+</node>
+</node>
+</node>
+</node>
+</map>
