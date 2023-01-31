774c18db353b661ca4d5e26f79abf314a75afa27
Benedikt Koeppel <be.public@gmail.com>
2023-01-31 21:55:37 +0100

    Update Google Analytics (and migrate to AndroidX as it is required)
    This reverts commit 6ca8bc560f2ec57d416b18003dfb69e76c1ffb3e.

--- app/src/main/java/ch/benediktkoeppel/code/droidplane/NodeColumn.java
+++ app/src/main/java/ch/benediktkoeppel/code/droidplane/NodeColumn.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Point;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.*;
 import android.view.ContextMenu.ContextMenuInfo;
