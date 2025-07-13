b1b7207fa23f0a54d0d8a19d27ad94d45337a735
Benedikt Koeppel <be.public@gmail.com>
2025-07-13 10:12:03 +0200

    Reverted back to old layout (not edge-to-edge)

--- app/src/main/java/ch/benediktkoeppel/code/droidplane/MainActivity.java
+++ app/src/main/java/ch/benediktkoeppel/code/droidplane/MainActivity.java
@@ -25,6 +25,10 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.Insets;
 
 /**
  * The MainActivity can be started from the App Launcher, or with a File Open intent. If the MainApplication was
@@ -63,6 +67,14 @@ public class MainActivity extends FragmentActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Edge-to-edge: disable decor fitting system windows
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
+            Insets sysBars = insets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+            v.setPadding(sysBars.left, sysBars.top, sysBars.right, sysBars.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
 
         // initialize android stuff
         // Google Analytics
@@ -435,7 +447,7 @@ public class MainActivity extends FragmentActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
-
+        super.onActivityResult(requestCode, resultCode, resultData);
         // The ACTION_OPEN_DOCUMENT intent was sent with the request code READ_REQUEST_CODE. If the request code seen
         // here doesn't match, it's the response to some other intent, and the code below shouldn't run at all.
 
